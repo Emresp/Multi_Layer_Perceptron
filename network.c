ce3cbd1d8e7e0c5c6db23c965296b3315cc2fec9
Emre Ayözcan <emreyozcan16@gmail.com>
2026-05-16 18:52:55 +0300

    feat: oluşan katmanları bir arada tutup bağlantılarını kolay sağlamk için fonksiyonlar bildirildi ve tanımlamndı.

--- network.c
+++ network.c
@@ -1,5 +1,23 @@
-//
-// Created by Emre on 10.05.2026.
-//
-
 #include "network.h"
+#include <stdlib.h>
+
+Network* create_network()
+{
+    Network* network = (Network*)malloc(sizeof(Network));
+
+    network->layer_count=0;
+    network->layers=NULL;
+
+    return network;
+}
+
+void add_layer(Layer* layer,Network* network)
+{
+    //Yeni katman adresi eklenceği için bellekte yer açtık şu anki
+    network->layers=(Layer**)realloc(network->layers,(network->layer_count + 1) * sizeof(Layer*));
+
+    network->layers[network->layer_count]=layer;
+
+    network->layer_count=network->layer_count+1;
+}
+
--- network.h
+++ network.h
@@ -1,8 +1,20 @@
-//
-// Created by Emre on 10.05.2026.
-//
-
 #ifndef MLP_CORE_NETWORK_H
 #define MLP_CORE_NETWORK_H
+#include "layer.h"
+
+//Oluşturmuş olduğumuz katmanların karışmasını önlemek ve kullanılabilirliği artırmak için ağdaki tüm katmanları ve katman sayısını bir arada tutar
+typedef struct
+{
+    int layer_count;
+    Layer** layers;
+
+}Network;
+
+//Ana ağ yapısını oluşturmak için
+Network* create_network();
+
+//Oluşturmuş olduğumuz katmanlarız ağ eklemek için gerekli fonksiyon. 2 adet paraemtere alır ağa eklencek katmanın adresi ve eklendiği ağ adresi
+void add_layer(Layer* layer,Network* network);
+
 
 #endif //MLP_CORE_NETWORK_H
